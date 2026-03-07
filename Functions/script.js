cdb41ad940e648aa361d0287c3ef4b8069657ca7
Rakibul Islam Sadi <rakib276661@gmail.com>
2026-03-07 19:37:18 +0600

    I learned some functions

--- Functions/script.js
+++ Functions/script.js
@@ -1,3 +1,5 @@
+// Function Statement
+
 function abcd() {
   console.log("Rakibul Islam");
   console.log("Rakibul Islam");
@@ -8,3 +10,68 @@ function abcd() {
 }
 
 abcd();
+
+// Function Expression
+
+let rakib = function (a) {
+  console.log("Rakibul Islam Sadi " + a);
+}
+
+rakib(0);
+
+// Fat arrow Function
+
+let sadi = () => {
+  console.log("Rakibul Sadi");
+}
+
+sadi();
+//_________________________________________________________________________________________________________
+
+function fn(a = "Sadi") {
+  console.log(`${a} is singing`);
+}
+
+fn();
+fn("Sani");
+fn("Yasin");
+
+//______________________________________________________________________________________________________________
+
+
+function add(v1 = 0, v2 = 0) {
+  console.log(v1 + v2);
+}
+
+add(3, 4);
+add();
+
+//--------------------------------------------------------------------------------------------------------------------
+
+
+function pqr(...val) {
+  console.log(val)
+}
+
+pqr(1,2,3,4,5,6,7,8,9,0);
+
+//------------------------------------------------------------------------------------------------------------------------
+
+function meow(a1, a2) {
+return a1 + a2;  
+}
+
+
+let val = meow(20, 20);
+
+console.log(val);
+
+// first class functon----------------------------------------------------------------------------------
+
+function fc(val) {
+  val();
+}
+
+fc(function () {
+  console.log("This is Rakibul Islam") // we can treat function as value
+});
