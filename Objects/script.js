1b25d9ec461a1e508ac70c5d69f3e3ec28f6ab22
Rakibul Islam Sadi <rakib276661@gmail.com>
2026-03-28 23:07:57 +0600

    changed

--- Objects/script.js
+++ Objects/script.js
@@ -6,7 +6,6 @@
 // console.log(obj.name);
 // console.log(obj["name"]);
 
-
 // for (let sadi in obj) {
 //   console.log(sadi, obj[sadi]);
 // }
@@ -32,11 +31,10 @@
 // obj.name = "Rakib";
 // console.log(obj);
 
-
 let car = {
   brand: "Toyota",
   model: "Corolla",
-  year: 2020
+  year: 2020,
 };
 
 car.year = 2022;
@@ -47,9 +45,6 @@ console.log(car.model);
 delete car.model;
 console.log(car.model);
 
-
-
-
 // let student = {
 //   name: "Rahim",
 //   age: 15,
@@ -59,9 +54,6 @@ console.log(car.model);
 // for(let key in student) {
 //   console.log(key, student[key]);
 // }
-
-
-
 
 // let calculator = {
 //   num1: 5,
@@ -75,32 +67,23 @@ console.log(car.model);
 // // add()
 // // subtract()   // you didn't teach me.
 
-
-
-
 let result = {
   name: "Rakibul Islam",
   marks: {
     math: 80,
     english: 95,
-    science: 70
-  }
-}
+    science: 70,
+  },
+};
 console.log(result.marks.math);
-
-
-
 
 let obj = {
   name: "Sadi",
-  age: 19
-}
+  age: 19,
+};
 
 console.log(Object.keys(obj));
 console.log(Object.values(obj));
-
-
-
 
 // let user = {
 // //   name: "Your Name",
@@ -113,9 +96,6 @@ console.log(Object.values(obj));
 // // login() method → isLoggedIn = true
 // // logout() method → isLoggedIn = false
 // // status() → console.log("Logged in" / "Logged out")   <--- You asked but didn't teach.
-
-
-
 
 // let school = {
 //   students: [
@@ -130,28 +110,21 @@ console.log(Object.values(obj));
 
 // }
 
-
-
 let user = {
   name: "Hiyashi",
   status: "Active",
-  changeStatus: function(updt) {
+  changeStatus: function (updt) {
     this.status = updt;
   },
 
-  showProfile: function() {
-    console.log(`User: ${this.name} is currently ${this.status}`)
-  }
-
-}
+  showProfile: function () {
+    console.log(`User: ${this.name} is currently ${this.status}`);
+  },
+};
 
 user.showProfile();
 user.changeStatus("Offline");
 user.showProfile();
-
-
-
-
 
 // let cart = {
 //   owner: "Hiyashi",
@@ -174,8 +147,6 @@ user.showProfile();
 // cart.totalItems();
 // cart.showCart();
 
-
-
 let hero = {
   name: "Hiyashi",
   health: 100,
@@ -189,20 +160,20 @@ let hero = {
   heal() {
     if (this.health === 100) {
       return this.health;
-    } else if  (this.health >= 91) {
+    } else if (this.health >= 91) {
       this.health = 100;
     } else {
       this.health += 10;
     }
   },
-  checkStatus () {
+  checkStatus() {
     if (this.health <= 0) {
       console.log(`Game Over! ${this.name} is defeated.`);
     } else {
       console.log(`${this.name} has ${this.health} HP left.`);
     }
-  }
-}
+  },
+};
 
 hero.attack();
 hero.takeDamage(84);
@@ -217,10 +188,6 @@ hero.heal();
 hero.heal();
 hero.heal();
 hero.checkStatus();
-
-
-
-
 
 let fan = {
   brand: "RFL",
@@ -239,7 +206,7 @@ let fan = {
       this.speed += 1;
       console.log(`Speed ${this.speed}`);
     } else {
-      console.log("This is max power")
+      console.log("This is max power");
     }
   },
 
@@ -249,16 +216,13 @@ let fan = {
     console.log("Fan is OFF");
   },
 
-  fanStatus () {
+  fanStatus() {
     let status = this.isOn ? "ON" : "OFF";
     console.log(`The fan is ${status} and speed ${this.speed}`);
-  }
-}
+  },
+};
 
 fan.fanStatus();
-
-
-
 
 let account = {
   holderName: "Hiyashi",
@@ -266,23 +230,28 @@ let account = {
 
   deposit(amount) {
     this.balance += amount;
-    console.log(`Deposited: ${amount} TK. New Balance: ${this.balance} TK.`)
+    console.log(`Deposited: ${amount} TK. New Balance: ${this.balance} TK.`);
   },
 
   withdraw(amount) {
     if (amount > this.balance) {
-      console.log(`Insufficient Balance! You can withdraw maximum ${this.balance} TK.`);
-    }else {
+      console.log(
+        `Insufficient Balance! You can withdraw maximum ${this.balance} TK.`,
+      );
+    } else {
       this.balance -= amount;
-      console.log(`Withdraw: ${amount} TK. Remaining Balance: ${this.balance} TK.`);
+      console.log(
+        `Withdraw: ${amount} TK. Remaining Balance: ${this.balance} TK.`,
+      );
     }
   },
 
   checkBalance() {
-    console.log(`Your name is ${this.holderName} and your current balance is ${this.balance} TK.`);
-  }
-
-}
+    console.log(
+      `Your name is ${this.holderName} and your current balance is ${this.balance} TK.`,
+    );
+  },
+};
 
 account.deposit(500);
 account.checkBalance();
@@ -291,31 +260,30 @@ account.checkBalance();
 account.withdraw(400);
 account.withdraw(200);
 
-
-
 let student = {
   name: "Hiyashi",
   details: {
     age: 19,
-    city: "Jashore"
+    city: "Jashore",
   },
   marks: {
     math: 90,
-    english: 100
+    english: 100,
   },
 
   showInfo() {
-    console.log(`${this.name} lives in ${this.details.city} and got ${this.marks.math} in Math and ${this.marks.english} in English`);
-  }
-}
+    console.log(
+      `${this.name} lives in ${this.details.city} and got ${this.marks.math} in Math and ${this.marks.english} in English`,
+    );
+  },
+};
 
 student.showInfo();
-
 
 let classroom = [
   { name: "Hiyashi", city: "Jashore", marks: 90 },
   { name: "Rakib", city: "Dhaka", marks: 85 },
-  { name: "Sany", city: "Khulna", marks: 95 }
+  { name: "Sany", city: "Khulna", marks: 95 },
 ];
 
 // for (let data of classroom) {
@@ -325,20 +293,17 @@ let classroom = [
 //     console.log(`${data.name}, Marks: ${data.marks}`);  }
 // }
 
-
 for (let data of classroom) {
-  if(data.city === "Dhaka") {
+  if (data.city === "Dhaka") {
     console.log(data);
   }
 }
 
-
-
 let myLibrary = [
-  {title: "Javascript", author: "Rakib", isRead: true},
-  {title: "Decipline", author: "Hiyashi", isRead: false},
-  {title: "Failer", author: "Sadi", isRead: false}
-]
+  { title: "Javascript", author: "Rakib", isRead: true },
+  { title: "Decipline", author: "Hiyashi", isRead: false },
+  { title: "Failer", author: "Sadi", isRead: false },
+];
 
 // let filterLibrary = myLibrary.filter((book) => {
 //   if (book.isRead === false) {
@@ -346,16 +311,14 @@ let myLibrary = [
 //   }
 // })
 
-let unRead = myLibrary.filter(book => book.isRead === false);
+let unRead = myLibrary.filter((book) => book.isRead === false);
 console.log(unRead);
 
-for(let book of myLibrary) {
+for (let book of myLibrary) {
   if (book.isRead === false) {
-    console.log(`You didn't read ${book.title} of ${book.author}.`)
+    console.log(`You didn't read ${book.title} of ${book.author}.`);
   }
 }
-
-
 
 // let school = {
 //   students: [
@@ -366,7 +329,6 @@ for(let book of myLibrary) {
 
 // console.log(school.students[0].name);
 // console.log(school.students[1].marks);
-
 
 // let school = {
 //   students: [
@@ -388,30 +350,28 @@ for(let book of myLibrary) {
 // school.showFirstStudent();
 // school.showAllStudents();
 
-
 let school = {
   students: [
     { name: "Rahim", marks: 80 },
-    { name: "Karim", marks: 65 }
+    { name: "Karim", marks: 65 },
   ],
 
   getAllNames() {
     let names = [];
-    for(let data of this.students) {
+    for (let data of this.students) {
       names.push(data.name);
-  }
-  return names;
+    }
+    return names;
   },
 
   addStudent(name, marks) {
-    this.students.push({name, marks})
+    this.students.push({ name, marks });
   },
 
   getAverageMarks() {
     let total = 0;
-    for(let data of this.students) {
+    for (let data of this.students) {
       total += data.marks;
-
     }
     return total / this.students.length;
   },
@@ -419,116 +379,104 @@ let school = {
   getTopStudent() {
     let top = this.students[0];
 
-    for(let data of this.students) {
+    for (let data of this.students) {
       if (data.marks > top.marks) {
         top = data;
       }
     }
     return top;
-  }
+  },
 };
 
 school.getAllNames();
 school.addStudent("Rakib", 70);
 school.getAllNames();
 
-
-console.log(school.students)
+console.log(school.students);
 
 school.getAverageMarks();
 school.getTopStudent();
 
 //_________________________________________________________________________________________
 
-
 let cart = [
   { product: "Laptop", price: 50000, quantity: 1 },
   { product: "Mouse", price: 1200, quantity: 2 },
-  { product: "Keyboard", price: 2500, quantity: 1 }
+  { product: "Keyboard", price: 2500, quantity: 1 },
 ];
 
 let total = 0;
 
-for(let data of cart) {
-  total = total + (data.price * data.quantity);
+for (let data of cart) {
+  total = total + data.price * data.quantity;
 }
-console.log(total)
-
+console.log(total);
 
 let movies = [
   { title: "Inception", rating: 8.8 },
   { title: "The Dark Knight", rating: 9.0 },
   { title: "Interstellar", rating: 8.6 },
   { title: "Fast X", rating: 6.5 },
-  { title: "Avatar", rating: 7.8 }
+  { title: "Avatar", rating: 7.8 },
 ];
 
-for(let data of movies) {
+for (let data of movies) {
   if (data.rating >= 8) {
     console.log(data.title);
   }
 }
 
+// let students = [
+//   { name: "Rakib", mark: 35 },
+//   { name: "Sany", mark: 28 },
+//   { name: "Hiyashi", mark: 45 },
+//   { name: "Sadi", mark: 32 },
+// ];
 
+// for (let data of students) {
+//   if (data.mark < 33) {
+//     console.log(`${data.name} has failed.`);
+//   } else {
+//     console.log(`${data.name} has passed.`);
+//   }
+// }
 
-let students = [
-  { name: "Rakib", mark: 35 },
-  { name: "Sany", mark: 28 },
-  { name: "Hiyashi", mark: 45 },
-  { name: "Sadi", mark: 32 }
-];
+// let products = [
+//   { name: "Phone", price: 20000 },
+//   { name: "Watch", price: 5000 },
+//   { name: "Headphone", price: 3000 }
+// ];
 
-for (let data of students) {
-  if (data.mark < 33) {
-    console.log(`${data.name} has failed.`);
-  } else {
-    console.log((`${data.name} has passed.`))
-  }
-}
-
-
-
-let products = [
-  { name: "Phone", price: 20000 },
-  { name: "Watch", price: 5000 },
-  { name: "Headphone", price: 3000 }
-];
-
-function filterExpensive(list, minPrice) {
-  for(let data of list) {
-    if(data.price >= minPrice) {
-      console.log(data)
-    }
-    }
-}
-filterExpensive(products, 4000);
-
-
+// function filterExpensive(list, minPrice) {
+//   for(let data of list) {
+//     if(data.price >= minPrice) {
+//       console.log(data)
+//     }
+//     }
+// }
+// filterExpensive(products, 4000);
 
 function makeSquare(number) {
-  return number * number
+  return number * number;
 }
 console.log(makeSquare(5));
 console.log(makeSquare(10));
 // let myResult = makeSquare(5);
 // console.log(myResult);
 
-
-
 let employees = [
   { name: "Rakib", salary: 50000 },
   { name: "Sany", salary: 30000 },
   { name: "Hiyashi", salary: 45000 },
   { name: "Sadi", salary: 25000 },
-  { name: "Siam", salary: 60000 }
+  { name: "Siam", salary: 60000 },
 ];
-
 
 function getHighSalary(list, limit) {
   let filterName = [];
   let nameList = [];
-  for(let data of list) {
-    if(data.salary > limit) {
+  for (let data of list) {
+    if (data.salary > limit) {
       filterName.push(data);
       nameList.push(data.name.toUpperCase());
     }
@@ -538,3 +486,115 @@ function getHighSalary(list, limit) {
 
 let newlist = getHighSalary(employees, 40000);
 console.log(newlist);
+
+const products = [
+  {
+    id: 101,
+    name: "Logitech Mouse",
+    price: 950,
+    category: "Electronics",
+    stock: 12,
+  },
+  {
+    id: 102,
+    name: "Mechanical Keyboard",
+    price: 2500,
+    category: "Electronics",
+    stock: 0,
+  },
+  {
+    id: 103,
+    name: "Cotton T-Shirt",
+    price: 450,
+    category: "Clothing",
+    stock: 25,
+  },
+  {
+    id: 104,
+    name: "Water Bottle",
+    price: 150,
+    category: "Lifestyle",
+    stock: 8,
+  },
+  {
+    id: 105,
+    name: "Gaming Headphone",
+    price: 1800,
+    category: "Electronics",
+    stock: 5,
+  },
+];
+
+function showProducts(list) {
+  for (let data of list) {
+    console.log(`Product Name: ${data.name} & Price: ${data.price} TK.`);
+  }
+}
+showProducts(products);
+
+function filterByPrice(list, sort) {
+  let filterPrice = [];
+  for (let data of list) {
+    if (data.price >= sort) {
+      filterPrice.push(data);
+    }
+  }
+  console.log(filterPrice);
+}
+filterByPrice(products, 500);
+
+function discountCalc(list) {
+  let discountPrice = list.map((item) => item.price - item.price * 0.1);
+  console.log(discountPrice);
+}
+discountCalc(products);
+
+function totalCost(list) {
+  let total = list.reduce((acc, crr) => acc + crr.price * crr.stock, 0);
+  console.log(total);
+}
+totalCost(products);
+
+function stockCheck(list, product) {
+  let findStock = list.find((item) => item.name === product);
+  if (findStock.stock === 0) {
+    console.log("out");
+  } else {
+    console.log("available");
+  }
+}
+
+stockCheck(products, "Gaming Headphone");
+
+const students = [
+  { id: 1, name: "Rakib", presentDays: 28, totalClasses: 30 },
+  { id: 2, name: "Sany", presentDays: 18, totalClasses: 30 },
+  { id: 3, name: "Arif", presentDays: 10, totalClasses: 30 },
+  { id: 4, name: "Sadi", presentDays: 23, totalClasses: 30 },
+  { id: 5, name: "Rony", presentDays: 5, totalClasses: 30 },
+];
+
+function checkExamEligibility(list, name) {
+  let studentData = list.find((item) => item.name === name);
+  let percentage = (studentData.presentDays / studentData.totalClasses) * 100;
+  if (!percentage) {
+    return;
+  } else if (percentage >= 75) {
+    console.log(`Name: ${studentData.name} - Exam Allowed.`);
+  } else if (percentage >= 50 && percentage <= 74) {
+    console.log(`Name: ${studentData.name} - Exam Allowed with FIne.`);
+  } else {
+    console.log(`Name: ${studentData.name} - Exam Not Allowed`);
+  }
+}
+
+checkExamEligibility(students, "Rakib");
+
+
+function totalPercentage(list) {
+  let alldetails = list.map(item => (item.presentDays / item.totalClasses) * 100);
+  console.log(alldetails);
+    
+}
+
+totalPercentage(students);
